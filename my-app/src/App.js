686cd88f42aa0dd852aa441501a0e05365662b7c
Serhii Shevchenko <serhii.shevchenko@tmx.com.ua>
2021-03-11 19:16:26 +0200

    11/03/21

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import s from './App.module.css';
 
 
 const App = () => {
   return (
-    <div className="app-wrapper">
+    <div className={s.appWrapper}>
       <Header />
       <Navbar />
-      <Profile />
+      <div className={s.appWrapperContent}>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   )
 }
