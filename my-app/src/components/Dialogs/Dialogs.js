cd10eccc67a466f500217927e864e6cd1ea79cd2
Serhii Shevchenko <serhii.shevchenko@tmx.com.ua>
2021-03-12 15:41:12 +0200

    12/03/21

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -2,8 +2,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <section className={s.dialogsItem}>
+        <div className={`${s.dialog} ${s.active}`}>
+          Alena
+        </div>
+        <div className={s.dialog}>
+          Uriy
+        </div>
+        <div className={s.dialog}>
+          Valeria
+        </div>
+      </section>
+      <section className={s.messages}>
+        <div className={s.massage}>
+          Hi!
+        </div>
+        <div className={s.massage}>
+          How are you?
+        </div>
+        <div className={s.massage}>
+          Nice!
+        </div>
+      </section>
     </div>
   )
 }
--- my-app/src/components/Profile/Profile.js
+++ my-app/src/components/Profile/Profile.js
@@ -4,15 +4,15 @@ import s from './Profile.module.css';
 const Profile = () => {
   return (
     <main className={s.content}>
-    <div className={s.wallPaper}>
-      <img src="https://illustrators.ru/uploads/user/cover/163787/main_ytj-normal-750-300.png"/>
-    </div>
-    <div>
-      ava + description
-    </div>
-    <MyPosts/>
-  </main>
+      <div className={s.wallPaper}>
+        <img src="https://illustrators.ru/uploads/user/cover/163787/main_ytj-normal-750-300.png" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <MyPosts />
+    </main>
   )
-} 
+}
 
 export default Profile;
