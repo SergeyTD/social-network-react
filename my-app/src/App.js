0bce8b96debb46682100702c05f75b69ff003c73
Serhii Shevchenko <serhii.shevchenko@tmx.com.ua>
2021-03-12 10:15:59 +0200

    12/03/21

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,29 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import s from './App.module.css';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Setting from './components/Setting/Setting';
+
 
 
 const App = () => {
   return (
-    <div className={s.appWrapper}>
-      <Header />
-      <Navbar />
-      <div className={s.appWrapperContent}>
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className={s.appWrapper}>
+        <Header />
+        <Navbar />
+        <div className={s.appWrapperContent}>
+          <Route path="/profile" component={ Profile } />
+          <Route path="/dialogs" component={ Dialogs } />
+          <Route path="/news" component={ News } />
+          <Route path="/music" component={ Music } />
+          <Route path="/setting" component={ Setting } />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
