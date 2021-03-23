ea16fa130d1119a44130bdb2c13f22ec3ab0feb3
Serhii Shevchenko <serhii.shevchenko@tmx.com.ua>
2021-03-23 20:50:56 +0200

    23/03/2021

--- my-app/src/components/Dialogs/Message/Message.js
+++ my-app/src/components/Dialogs/Message/Message.js
@@ -3,6 +3,7 @@ import s from './Message.module.css';
 const Message = (props) => {
   return (
     <div className={s.message}>
+      <img src="https://static.ukrinform.com/photos/2020_02/thumb_files/630_360_1580657832-217.png" />
       {props.message}
     </div>
   )
